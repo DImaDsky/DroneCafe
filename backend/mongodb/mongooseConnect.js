8f00844439c140505889631d0594293e07fd501d
dimadsky@gmail.com <dimadsky@gmail.com>
2017-07-10 16:13:06 +0300

    fix heroku

--- backend/mongodb/mongooseConnect.js
+++ backend/mongodb/mongooseConnect.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+//heroku config:get MONGODB_URI
+const heroku_url = 'mongodb://heroku_7bgdqq7d:eo2nlajh0bqhgr6jl9ccrdlgam@ds153652.mlab.com:53652/heroku_7bgdqq7d';
+
 const url = 'mongodb://localhost:27017/droneCafe';
-mongoose.connect(url);
+mongoose.connect(heroku_url);
 let db = mongoose.connection;
 db.on('error', err => {
     console.log(err);
